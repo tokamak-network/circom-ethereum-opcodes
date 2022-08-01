f5f21aaffeaf7e931317330d083673461f41b9a1
Youn <pleiadex.dev@gmail.com>
2022-08-01 14:13:15 +0900

    tested arithmetic operators

--- test/arithmetic_op.js
+++ test/arithmetic_op.js
@@ -25,16 +25,22 @@ describe("0x01 Add test", function ()  {
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] + input[1])))
   })
   it("Should equal to sum of two big enough inputs", async() => {
-    const input = [14474011154664524427946373126085988481658748083205070504932198000989141204992, 14474011154664524427946373126085988481658748083205070504932198000989141204992]
+    const input = [
+      Scalar.fromString("14474011154664524427946373126085988481658748083205070504932198000989141204992"), 
+      Scalar.fromString("14474011154664524427946373126085988481658748083205070504932198000989141204992")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] + input[1])))
   })
   it("Should equal to zero", async() => {
-    const input = [Scalar.fromString("1"), Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616")] // 1, p - 1는 값이 너무 커서 값이 제대로 안 나옴.
+    const input = [
+      Scalar.fromString("1"),
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
-    assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] + input[1])))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
   })
 })
 
@@ -52,13 +58,19 @@ describe("0x02 Mul test", function ()  {
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] * input[1])))
   })
   it("Should equal to product of two big enough inputs", async() => {
-    const input = [14474011154664524427946373126085988481658748083205070504932198000989141204992, 81221]
+    const input = [
+      Scalar.fromString("14474011154664524427946373126085988481658748083205070504932198000989141204992"),
+      Scalar.fromString("81221")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] * input[1])))
   })
   it("Should equal to zero", async() => {
-    const input = [21888242871839275222246405745257275088548364400416034343698204186575808495616, 0]
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616"),
+      Scalar.fromString("0")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
@@ -83,7 +95,10 @@ describe("0x03 Sub test", function ()  {
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(input[0] - input[1])))
   })
   it("Should equal to zero", async() => {
-    const input = [21888242871839275222246405745257275088548364400416034343698204186575808495616, 21888242871839275222246405745257275088548364400416034343698204186575808495616]
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616"), 
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
@@ -109,7 +124,10 @@ describe("0x04 Div test", function ()  {
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
   })
   it("Should equal to one", async() => {
-    const input = [21888242871839275222246405745257275088548364400416034343698204186575808495616, 21888242871839275222246405745257275088548364400416034343698204186575808495616]
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616"), 
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616")
+    ]
     witness = await circuit.calculateWitness({"in": input}, true)
     assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
     assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)))
@@ -118,14 +136,126 @@ describe("0x04 Div test", function ()  {
 
 // TODO: 0x05 SDIV
 
-// TODO: 0x06 MOD
+describe("0x06 MOD test", function ()  {
+  let circuit;
+  let witness;
+  before( async () => {
+    circuit = await wasm_tester(path.join(__dirname, "circuits", "mod_test.circom"))
+  })
+  it("Should equal to dividend", async() => {
+    const input = [9, 20]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), input[0] % input[1]))
 
+  })
+  it("Should equal to zero", async() => {
+    const input = [0, 1000]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+  it("Should equal to zero", async() => {
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616"), 
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616")
+    ]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+})
 // TODO: 0x07 SMOD
 
-// TODO: 0x08 ADDMOD
+describe("0x08 ADDMOD test", function ()  {
+  let circuit;
+  let witness;
+  before( async () => {
+    circuit = await wasm_tester(path.join(__dirname, "circuits", "addmod_test.circom"))
+  })
+  it("Should equal to modular of summation of first two inputs", async() => {
+    const input = [9, 20, 7]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e((input[0] + input[1]) % input[2])))
+  })
+  it("Should equal to zero", async() => {
+    const input = [0, 0, 7]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+  it("Should equal to zero", async() => {
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495616"),
+      Scalar.fromString("1"),
+      Scalar.fromString("17")
+    ]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+})
 
-// TODO: 0x09 MULMOD
+describe("0x09 MULMOD test", function ()  {
+  let circuit;
+  let witness;
+  before( async () => {
+    circuit = await wasm_tester(path.join(__dirname, "circuits", "mulmod_test.circom"))
+  })
+  it("Should equal to modular of multiplication of first two inputs", async() => {
+    const input = [9, 20, 7]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e((input[0] * input[1]) % input[2])))
+  })
+  it("Should equal to zero", async() => {
+    const input = [0, 0, 7]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+  it("Should equal to zero", async() => {
+    const input = [
+      Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617"),
+      Scalar.fromString("1"),
+      Scalar.fromString("17")
+    ]
+    witness = await circuit.calculateWitness({"in": input}, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+})
 
-// TODO: 0x0A EXP
-
+describe("0x0A EXP test", function ()  {
+  let circuit;
+  let witness;
+  before( async () => {
+    circuit = await wasm_tester(path.join(__dirname, "circuits", "exp_test.circom"))
+  })
+  it("Should equal to pow(a, b)", async() => {
+    const input = {in: 9, n: 5}
+    witness = await circuit.calculateWitness(input, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(Math.pow(input["in"], input["n"]))))
+  })
+  it("Should equal to one", async() => {
+    const input = {in: 1, n: 100}
+    witness = await circuit.calculateWitness(input, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)))
+  })
+  it("Should equal to one", async() => {
+    const input = {in: 17, n: 0}
+    witness = await circuit.calculateWitness(input, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(1)))
+  })
+  it("Should equal to zero", async() => {
+    const input = {in: 0, n: 100}
+    witness = await circuit.calculateWitness(input, true)
+    assert(Fr.eq(Fr.e(witness[0]), Fr.e(1)))
+    assert(Fr.eq(Fr.e(witness[1]), Fr.e(0)))
+  })
+})
 // TODO: 0x0B SINGEXTEND
