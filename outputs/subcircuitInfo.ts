71efb64133c5da06022a42e5e1f458fb1f1547c8
JehyukJang <cjhyuck213@gmail.com>
2024-12-15 12:14:14 +0800

    Minor change in the output file names

--- outputs/subcircuitInfo.ts
+++ outputs/subcircuitInfo.ts
@@ -3,391 +3,40 @@ export const subcircuits =
   {
     "id": 0,
     "name": "Load",
-    "Nwires": 513,
+    "Nwires": 8,
     "Out_idx": [
       1,
-      256
+      6
     ],
     "In_idx": [
-      257,
-      0
-    ]
-  },
-  {
-    "id": 1,
-    "name": "ADD",
-    "Nwires": 293,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 2,
-    "name": "MUL",
-    "Nwires": 591,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 3,
-    "name": "SUB",
-    "Nwires": 293,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 4,
-    "name": "DIV",
-    "Nwires": 1234,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 5,
-    "name": "SDIV",
-    "Nwires": 4774,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 6,
-    "name": "MOD",
-    "Nwires": 1234,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 7,
-    "name": "SMOD",
-    "Nwires": 4774,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 8,
-    "name": "ADDMOD",
-    "Nwires": 1674,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 9,
-    "name": "MULMOD",
-    "Nwires": 2555,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 10,
-    "name": "EQ",
-    "Nwires": 25,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 11,
-    "name": "ISZERO",
-    "Nwires": 13,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
+      7,
+      4
     ]
   },
   {
     "id": 12,
     "name": "SHL",
-    "Nwires": 421,
+    "Nwires": 20,
     "Out_idx": [
       1,
-      2
+      18
     ],
     "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 13,
-    "name": "SHR",
-    "Nwires": 421,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 14,
-    "name": "LT",
-    "Nwires": 290,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 15,
-    "name": "GT",
-    "Nwires": 300,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 16,
-    "name": "NOT",
-    "Nwires": 1033,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 17,
-    "name": "BYTE",
-    "Nwires": 389,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 18,
-    "name": "SAR",
-    "Nwires": 1355,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 19,
-    "name": "SIGNEXTEND",
-    "Nwires": 3317,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 20,
-    "name": "SLT",
-    "Nwires": 578,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 21,
-    "name": "SGT",
-    "Nwires": 588,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 22,
-    "name": "AND",
-    "Nwires": 1553,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
-    ]
-  },
-  {
-    "id": 23,
-    "name": "OR",
-    "Nwires": 1553,
-    "Out_idx": [
-      1,
-      2
-    ],
-    "In_idx": [
-      3,
-      0
+      19,
+      16
     ]
   },
   {
     "id": 24,
     "name": "XOR",
-    "Nwires": 1553,
+    "Nwires": 0,
     "Out_idx": [
       1,
-      2
-    ],
-    "In_idx": [
-      3,
       0
-    ]
-  },
-  {
-    "id": 25,
-    "name": "DecToBit",
-    "Nwires": 258,
-    "Out_idx": [
-      1,
-      256
     ],
     "In_idx": [
-      257,
-      0
-    ]
-  },
-  {
-    "id": 26,
-    "name": "SubEXP",
-    "Nwires": 1220,
-    "Out_idx": [
       1,
-      4
-    ],
-    "In_idx": [
-      5,
-      0
-    ]
-  },
-  {
-    "id": 27,
-    "name": "KeccakBufferIn",
-    "Nwires": 513,
-    "Out_idx": [
-      1,
-      256
-    ],
-    "In_idx": [
-      257,
-      0
-    ]
-  },
-  {
-    "id": 28,
-    "name": "KeccakBufferOut",
-    "Nwires": 513,
-    "Out_idx": [
-      1,
-      256
-    ],
-    "In_idx": [
-      257,
-      256
-    ]
-  },
-  {
-    "id": 29,
-    "name": "ReturnBuffer",
-    "Nwires": 513,
-    "Out_idx": [
-      1,
-      256
-    ],
-    "In_idx": [
-      257,
-      0
+      28
     ]
   }
 ]
